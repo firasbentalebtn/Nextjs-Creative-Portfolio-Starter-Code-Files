8b5ad9e331c91ba1bda2f0743789269588dde612
Firas Bentaleb <firasbentalebdev@gmail.com>
2024-05-17 11:43:48 +0100

    avatar model

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,17 @@
 import Image from "next/image";
 import  bg  from "../../public/background/home-background.png"
+import RenderModel from "@/components/RenderModel";
+import Wizard, {Avatar} from "@/components/models/Avatar";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between  relative">
-        <Image src={bg} alt="background-image" className="w-full h-full object-cover object-center opacity-25" />
+        <Image src={bg} alt="background-image" fill className="w-full h-full object-cover object-center opacity-25" />
+        <div className="w-full h-screen">
+            {/* nav 3d model*/}
+            <RenderModel>
+                <Wizard/>
+            </RenderModel>
+        </div>
     </main>
   );
 }
